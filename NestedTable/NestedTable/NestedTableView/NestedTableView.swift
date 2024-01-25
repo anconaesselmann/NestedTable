6e6a462bec54d1d1e78da3ce037a728078b5ea29
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-01-25 14:53:21 -0800

    refactor

--- NestedTable/NestedTable/NestedTableView/NestedTableView.swift
+++ NestedTable/NestedTable/NestedTableView/NestedTableView.swift
@@ -26,39 +26,37 @@ struct NestedTableView<Content>: View {
     internal var contextMenuItems: [any ContextMenuItems] = DefaultContextMenuItems.allCases
 
     var body: some View {
-        VStack {
-            Table(of: BaseRow<Content>.self, selection: $vm.selection, sortOrder: $vm.sortOrder) {
-                TableColumn("Name", sortUsing: KeyPathComparator(\BaseRow.item.text, comparator: Comparator<String>())) { item in
-                    NameColumn(item: item, vm: vm)
-                }
-            } rows: {
-                ForEach(vm.items) { item in
-                    TableRow(item)
+        Table(of: BaseRow<Content>.self, selection: $vm.selection, sortOrder: $vm.sortOrder) {
+            TableColumn("Name", sortUsing: KeyPathComparator(\BaseRow.item.text, comparator: Comparator<String>())) { item in
+                NameColumn(item: item, vm: vm)
+            }
+        } rows: {
+            ForEach(vm.items) { item in
+                TableRow(item)
 #if os(macOS)
-                        .itemProvider {
-                            vm.itemProvider(for: item)
+                    .itemProvider {
+                        vm.itemProvider(for: item)
+                    }
+                    .if(item.isGroup) {
+                        $0.dropDestination(for: Data.self) {
+                            vm.itemsDropped($0, into: item.id)
                         }
-                        .if(item.isGroup) {
-                            $0.dropDestination(for: Data.self) {
-                                vm.itemsDropped($0, into: item.id)
-                            }
-                        }
+                    }
 #endif
-                }
             }
-            .contextMenu(forSelectionType: UUID.self) {
-                ItemContextMenu(
-                    vm,
-                    ids: $0,
-                    elements: contextMenuItems,
-                    contextMenuElementBuilder: contextMenuElementBuilder
-                )
-            } primaryAction: { items in
-                vm.primaryAction(items)
-            }
-            .onAppear {
-                vm.fetch()
-            }
+        }
+        .contextMenu(forSelectionType: UUID.self) {
+            ItemContextMenu(
+                vm,
+                ids: $0,
+                elements: contextMenuItems,
+                contextMenuElementBuilder: contextMenuElementBuilder
+            )
+        } primaryAction: { items in
+            vm.primaryAction(items)
+        }
+        .onAppear {
+            vm.fetch()
         }
     }
 }
