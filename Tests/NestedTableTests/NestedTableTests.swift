e20dfd277659f712f55ef881bb128ec3d319ef27
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-01-26 15:28:05 -0800

    Move example project to package

---
+++ Tests/NestedTableTests/NestedTableTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import NestedTable
+
+final class NestedTableTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
